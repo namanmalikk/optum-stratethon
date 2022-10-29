8bf4c2bb610cf84bd10119169d8a3b19b855f5cb
Naman Malik <namanmalikk@gmail.com>
2022-10-29 15:28:29 +0530

    basic setup with dark mode toggle topbar

---
+++ src/data/sampleData.js
@@ -0,0 +1,12 @@
+import { tokens } from "../theme";
+
+export const mockDataTeam = [
+  {
+    id: 1,
+    name: "Jon Snow",
+    email: "jonsnow@gmail.com",
+    age: 35,
+    phone: "(665)121-5454",
+    access: "admin",
+  },
+]
